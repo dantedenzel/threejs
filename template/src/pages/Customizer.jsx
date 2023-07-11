6f24f82ed4e4985580065a20e15d7283abae26ad
danteburnett <danteburnett@gmail.com>
2023-07-10 22:14:45 -0400

    imports

--- template/src/pages/Customizer.jsx
+++ template/src/pages/Customizer.jsx
@@ -7,6 +7,7 @@ import state from '../store';
 import { download } from '../assets';
 import { downloadCanvastoImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
 
 const Customizer = () => {
   return (
