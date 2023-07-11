645962e3f9f82cc94c4d56af413d4cdc2d6b89de
danteburnett <danteburnett@gmail.com>
2023-07-10 22:59:24 -0400

    indexjsx imports

--- template/src/canvas/index.jsx
+++ template/src/canvas/index.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei';
 
-const Canvas = () => {
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+
+const CanvasModel = () => {
 return (
     <div>Canvas</div>
 )
