36ab2c2a936a39ac6af3dbf440d26cce89aa2ec9
danteburnett <danteburnett@gmail.com>
2023-07-10 22:13:28 -0400

    imports

--- template/src/pages/Customizer.jsx
+++ template/src/pages/Customizer.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { AnimatePresence, motion  } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import config from '../config/config';
+import state from '../store';
+import { download } from '../assets';
+import { downloadCanvastoImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 
 const Customizer = () => {
   return (
