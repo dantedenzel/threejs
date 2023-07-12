d01e8a7d12624970cab85b28e692498b812b0957
danteburnett <danteburnett@gmail.com>
2023-07-11 23:07:15 -0400

    noshirt

--- template/src/canvas/index.jsx
+++ template/src/canvas/index.jsx
@@ -1,4 +1,5 @@
-import { Canvas } from '@react-three/fiber'
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
 import { Environment, Center } from '@react-three/drei';
 
 import Shirt from './Shirt';
@@ -7,7 +8,18 @@ import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
 return (
-    <div>Canvas</div>
+    <Canvas>
+        <ambientLight intensity={0.5} />
+        <Environment preset="city" />
+
+        {/* <CameraRig> */}
+            {/* <Backdrop /> */}
+            <Center>
+                <Shirt />
+            </Center>
+        {/* </CameraRig> */}
+
+    </Canvas>
 )
 }
 
