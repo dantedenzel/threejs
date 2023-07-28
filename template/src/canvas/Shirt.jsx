d72980880e9bd0d37339580aefbe4589ec528b1f
danteburnett <danteburnett@gmail.com>
2023-07-28 00:04:51 -0400

    commit

--- template/src/canvas/Shirt.jsx
+++ template/src/canvas/Shirt.jsx
@@ -43,7 +43,7 @@ const Shirt = () => {
               rotation={[0, 0, 0]}
               scale={0.15}
               map={logoTexture}
-              // map-anisotropy={16} 
+              // map-anisotropy={16} fix
               depthTest={false}
               depthWrite={true}
 
