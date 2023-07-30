aacc0e281a2a0767f03348d8b8f1d0ea549b6ba9
danteburnett <danteburnett@gmail.com>
2023-07-30 16:31:03 -0400

    json

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json({ limit: " 50mb" }))
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: "Hello from DALE" })
+    res.status(200).json({ message: "Hello from DALL.E" })
 })
 
 app.listen(8080, () => console.log('Server has started on 8080'))
