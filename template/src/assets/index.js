5842642ef9fe5e7e55658fab8c54b43ba9da6ca4
danteburnett <danteburnett@gmail.com>
2023-08-13 19:25:50 -0400

    commit

--- template/src/assets/index.js
+++ template/src/assets/index.js
@@ -8,4 +8,4 @@ import stylishShirt from "./stylish-tshirt.png";
 
 export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
 
-// new imports can go in from the
+// new imports can go in from t
