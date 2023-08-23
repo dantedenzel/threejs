e07f4e7edfdcbf290a38036ae35ed597e4936aef
danteburnett <danteburnett@gmail.com>
2023-08-22 22:44:36 -0400

    commit

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -6,6 +6,18 @@ dotenv.config();
 
 const router = express.Router();
 
+// potential fix
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+// ends here...
 const config = new Configuration({
     apiKey: process.env['OPEN_AI_KEY'],
 });
